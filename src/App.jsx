cfe575ad017c488c9e36218f740ae299a3242d71
Dominique Chin-Jones <domecj13@gmail.com>
2022-11-07 09:26:39 -0500

    fix p tag

--- src/App.jsx
+++ src/App.jsx
@@ -9,30 +9,30 @@ const API = {
   return (
       <div className="app">
         <div className="app-container">
-            <div className="top">
-                <div className="location">
-                    <p>Rockville</p>
-                </div>
-                <div className="temperature">
-                    <h1>99&deg;F</h1>
-                </div>
-                <div className="description">
-                    <p>Clear</p>
-                </div>
+          <div className="top">
+            <div className="location">
+              <p>Rockville</p>
             </div>
-            <div className="bottom">
-                <div className="feels-like">
-                    <p>103&deg;F</p>
-                </div>
-                <div className="humidity">
-                    <p>5%</p>
-                </div>
-                <div className="wind-speed">
-                    2 MPH
-                </div>
+            <div className="temperature">
+              <h1>99&deg;F</h1>
             </div>
+            <div className="description">
+              <p>Clear</p>
+            </div>
+         </div>
+         <div className="bottom">
+           <div className="feels-like">
+             <p>103&deg;F</p>
+           </div>
+           <div className="humidity">
+             <p>5%</p>
+           </div>
+           <div className="wind-speed">
+             <p>2 MPH</p>
+           </div>
         </div>
       </div>
+    </div>
   );
 }
 
