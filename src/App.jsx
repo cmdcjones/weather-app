ef056a9c4f2adee2c2b64195d2ec8a26b3cc60de
Dominique Chin-Jones <domecj13@gmail.com>
2022-11-07 08:55:07 -0500

    fill app template

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 
 function App() {
 const API = {
@@ -6,7 +7,32 @@ const API = {
 };
 
   return (
-      {}
+      <div className="app">
+        <div className="app-container">
+            <div className="top">
+                <div className="location">
+                    <p>Rockville</p>
+                </div>
+                <div className="temperature">
+                    <h1>99&deg;F</h1>
+                </div>
+                <div className="description">
+                    <p>Clear</p>
+                </div>
+            </div>
+            <div className="bottom">
+                <div className="feels-like">
+                    <p>103&deg;F</p>
+                </div>
+                <div className="humidity">
+                    <p>5%</p>
+                </div>
+                <div className="wind-speed">
+                    <p>2 MPH</p>
+                </div>
+            </div>
+        </div>
+      </div>
   );
 }
 
