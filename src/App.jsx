0df16f0a348c1aa45035a7a601daa5dbaa6deb43
Dominique Chin-Jones <domecj13@gmail.com>
2022-11-07 08:31:02 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+const API = {
+    key: "87c67ec091fba53a311efe1fff2ebdfb",
+    baseUrl: "http://api.openweathermap.org/data/2.5/"
+};
+
+  return (
+      {}
+  );
+}
+
+export default App
