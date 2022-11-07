401b5fe45b5cbaf925850f2873d6945c7ed54a04
Dominique Chin-Jones <domecj13@gmail.com>
2022-11-07 11:16:35 -0500

    refactor KEY, URL; implement fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,72 @@
 import { useState } from 'react';
 
 function App() {
-const API = {
-    key: "87c67ec091fba53a311efe1fff2ebdfb",
-    baseUrl: "http://api.openweathermap.org/data/2.5/"
+const KEY = "58ec2497078a2bd00ef372b92ecbe4d0";
+
+const [data, setData] = useState({});
+const [location, setLocation] = useState('');
+
+const URL = 'https://api.openweathermap.org/data/2.5/weather?q='
+            + location + '&appid=' + KEY + '&units=imperial';
+
+const getLocationData = (event) => {
+    if (event.key === "Enter") {
+        try {
+            fetch(URL)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setData({
+                    location: data.name,
+                    temperature: data.main.temp.toFixed(0),
+                    description: data.weather[0].description,
+                    feelsLike: data.main.feels_like,
+                    humidity: data.main.humidity,
+                    windSpeed: data.wind.speed,
+                });
+            });
+            setLocation('');
+        } catch (error) {
+            console.log(error)
+        }
+    }
 };
 
   return (
       <div className="app">
+        <div className="get-location">
+          <input
+            value={location}
+            onChange={event => setLocation(event.target.value)}
+            onKeyPress={getLocationData}
+            placeholder="Enter a location..."
+            type="text"
+          />
+        </div>
         <div className="app-container">
           <div className="top">
             <div className="location">
-              <p>Rockville</p>
+              <p>{data.location}</p>
             </div>
             <div className="temperature">
-              <h1>99&deg;F</h1>
+              <h1>{data.temperature}&deg;F</h1>
             </div>
             <div className="description">
-              <p>Clear</p>
+              <p>{data.description}</p>
             </div>
          </div>
          <div className="bottom">
            <div className="feels-like">
-             <p>103&deg;F</p>
+             <p className="bold">103&deg;F</p>
+             <p className="small">Feels Like</p>
            </div>
            <div className="humidity">
-             <p>5%</p>
+             <p className="bold">5%</p>
+             <p className="small">Humidity</p>
            </div>
            <div className="wind-speed">
-             <p>2 MPH</p>
+             <p className="bold">2 MPH</p>
+             <p className="small">Wind Speed</p>
            </div>
         </div>
       </div>
